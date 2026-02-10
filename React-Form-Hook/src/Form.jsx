4c45ba0e12fbdd76709003cb90c1c33b42ab56a1
Anil kumar <9012kumaranil@gmail.com>
2026-02-10 21:07:53 +0530

    React-Form-Hook

--- React-Form-Hook/src/Form.jsx
+++ React-Form-Hook/src/Form.jsx
@@ -5,9 +5,26 @@ function Form() {
     defaultValues: {
       name: "Anil Kumar",
       email: "",
-      age: 18
+      age: 18,
+      social: {
+        facebook: "",
+        twitter: ""
+      },
+      phonenumber: ["", ""],
+      dob: new Date(),
     }
   });
+  // const { register, handleSubmit, control, formState } = useForm({
+  //   defaultValues: async () => {
+  //     const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  //     const data = await response.json();
+  //     return {
+  //       name: data.name,
+  //       email: data.email,
+  //       age: 18
+  //     }
+  //   }
+  // });
   const { errors } = formState;
 
   console.log(useForm());
@@ -69,6 +86,33 @@ function Form() {
             }
           )} />
         {errors.age && <p className="error">{errors.age?.message}</p>}
+        <br /><br />
+        <label htmlFor="dob">DOB</label>
+        <input type="date" id='dob'
+          {...register("dob", {
+            valueAsDate: true
+          }
+          )} />
+        <br /><br />
+        <label htmlFor="facebook">Facebook</label>
+        <input type="text" id='facebook'
+          {...register("social.facebook"
+          )} />
+        <br /><br />
+        <label htmlFor="twitter">Twitter</label>
+        <input type="text" id='twitter'
+          {...register("social.twitter"
+          )} />
+        <br /><br />
+        <label htmlFor="primary-phonenumber">Primary Phone Number</label>
+        <input type="text" id='primary-phonenumber'
+          {...register("phonenumber.0"
+          )} />
+        <br /><br />
+        <label htmlFor="secondary-phonenumber">Secondary Phone Number</label>
+        <input type="text" id='secondary-phonenumber'
+          {...register("phonenumber.1"
+          )} />
         <br /><br />
         <button type='submit'>Submit</button>
         <DevTool control={control} placement="top-left" />
