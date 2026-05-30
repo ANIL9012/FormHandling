5889efb4d37842b875ecfcfd9e93c24bffaa1dd2
Anil Kumar <9012kumaranil@gmail.com>
2026-05-30 21:51:23 +0530

    Yup-Form-Validation

--- Yup-Form-Validation/src/schema.js
+++ Yup-Form-Validation/src/schema.js
@@ -6,17 +6,17 @@ export const registerSchema = yup.object({
     .trim()
     .lowercase()
     .test("no-admin", "Admin is not allowed", (value) => value !== "admin"),
-  email: yup
-    .string()
-    .email("Invalid Email")
-    .required("Email Is required")
-    .test("check-gmail", "Gmail is  allready exists", async (value) => {
-      const res = await fetch(
-        `https://jsonplaceholder.typicode.com/users?email=${value}`,
-      );
-      const data = await res.json();
-      return data.length === 0;
-    }),
+  // email: yup
+  //   .string()
+  //   .email("Invalid Email")
+  //   .required("Email Is required")
+  //   .test("check-gmail", "Gmail is  allready exists", async (value) => {
+  //     const res = await fetch(
+  //       `https://jsonplaceholder.typicode.com/users?email=${value}`,
+  //     );
+  //     const data = await res.json();
+  //     return data.length === 0;
+  //   }),
   password: yup
     .string()
     .min(6, "Password must be 6 characters")
